65312424e9686544c658a88115a5559cbcede0a7
starix2436 <75304097+starix2436@users.noreply.github.com>
2021-12-21 00:03:38 +0530

    developers page

--- pages/developers.php
+++ pages/developers.php
@@ -1,28 +1,212 @@
-<!-- To display our deatails -->
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <style>
+      .card {
+		background-color:black;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        max-width: 400px;
+        margin: auto;
+        text-align: center;
+		color:white;
+        font-family: arial;
+		border-radius: 1rem;
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      }
 
-    <title>Hello, world!</title>
+      .rollno {
+        color: grey;
+        font-size: 27px;
+      }
+
+      a {
+        text-decoration: none;
+        font-size: 30px;
+        color: black;
+      }
+
+      button:hover,
+      a:hover {
+        opacity: 0.7;
+      }
+    </style>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  <?php
+  include('navbar.php');
+  ?>
+   
+    <main>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+      <div class="col">
+        <div class="card">
+		<br>
+          <img
+            src="carson.jpg"
+            style="
+              width: 80%;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;
+			  border-radius: 10rem;
+            "
+          />
+		  <br>
+          <h2>Carson Rodrigues</h2>
+          <p class="rollno">1914010</p>
+          <h4>Don Bosco College of Engineering</h4>
+          <div style="margin: 24px">
+			<a 
+				href="https://github.com/rodriguescarson" 
+				class="github"
+				><i class="fa fa-github-square" style="color:#FFFFFF"></i
+			></a>
+			&nbsp &nbsp
+            <a
+              href="https://www.linkedin.com/in/rodriguescarson/"
+              class="linkedin"
+              ><i class="fa fa-linkedin" style="color: #0e76a8"></i
+            ></a>
+			&nbsp &nbsp
+            <a href="https://api.whatsapp.com/send/?phone=917020286635"
+              ><i class="fa fa-whatsapp" style="color: #25d366"></i
+            ></a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+		<br>
+          <img
+            src="yash.jpg"
+            style="
+              width: 80%;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;
+			  border-radius: 10rem;
+            "
+          />
+          <br>
+          <h2>Yash Karapurkar</h2>
+          <p class="rollno">1914057</p>
+          <h4>Don Bosco College of Engineering</h4>
+          <div style="margin: 24px">
+			<a 
+				href="https://github.com/whyKara" 
+				class="github"
+				><i class="fa fa-github-square" style="color:#FFFFFF"></i
+			></a>
+			&nbsp &nbsp
+            <a
+              href="https://www.linkedin.com/in/yash-karapurkar-59b92b1b7/"
+              class="linkedin"
+              ><i class="fa fa-linkedin" style="color: #0e76a8"></i
+            ></a>
+			&nbsp &nbsp
+            <a href="https://api.whatsapp.com/send/?phone=917264964649"
+              ><i class="fa fa-whatsapp" style="color: #25d366"></i
+            ></a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+		<br>
+          <img
+            src="nihal.jfif"
+            style="
+              width: 80%;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;
+			  border-radius: 10rem;
+            "
+          />
+          <br>
+          <h2>Nihal Kamat</h2>
+          <p class="rollno">1914024</p>
+          <h4>Don Bosco College of Engineering</h4>
+          <div style="margin: 24px">
+			<a 
+				href="https://github.com/starix2436" 
+				class="github"
+				><i class="fa fa-github-square" style="color:#FFFFFF"></i
+			></a>
+			&nbsp &nbsp
+            <a
+              href="https://www.linkedin.com/in/nihalkamat2436/"
+              class="linkedin"
+              ><i class="fa fa-linkedin" style="color: #0e76a8"></i
+            ></a>
+			&nbsp &nbsp
+            <a href="https://api.whatsapp.com/send/?phone=919405920393"
+              ><i class="fa fa-whatsapp" style="color: #25d366"></i
+            ></a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+		<br>
+          <img
+            src="histon.jpg"
+            style="
+              width: 80%;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;
+			  border-radius: 10rem;
+            "
+          />
+          <br>
+          <h2>Histon Pango</h2>
+          <p class="rollno">1914029</p>
+          <h4>Don Bosco College of Engineering</h4>
+          <div style="margin: 24px">
+			<a 
+				href="https://github.com/Hestonpango" 
+				class="github"
+				><i class="fa fa-github-square" style="color:#FFFFFF"></i
+			></a> 
+			&nbsp &nbsp
+            <a
+              href="https://www.linkedin.com/in/histon-pango-34797718b/"
+              class="linkedin"
+              ><i class="fa fa-linkedin" style="color: #0e76a8"></i
+            ></a>
+			&nbsp &nbsp
+            <a href="https://api.whatsapp.com/send/?phone=918379080567"
+              ><i class="fa fa-whatsapp" style="color: #25d366"></i
+            ></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="container" style="padding: 0rem; margin:0rem;">
 
-    <!-- Optional JavaScript; choose one of the two! -->
+        <?php
+  include('footer.php');
+?>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
+      </footer>
+  </main>
+  <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
   </body>
 </html>
