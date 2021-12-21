ece8c1008481fb4e1b1cdff779c2863e22c62b4b
Carson <rodriguescarson@hotmail.com>
2021-12-21 09:26:34 +0530

    fix: developer pages

--- pages/developers.php
+++ pages/developers.php
@@ -13,16 +13,40 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     />
-    
+    <style>
+      #card {
+		background-color:black;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        max-width: 400px;
+        margin: auto;
+        text-align: center;
+		color:white;
+        font-family: arial;
+		border-radius: 1rem;
+
+      }
+
+      #rollno {
+        color: grey;
+        font-size: 27px;
+      }
+
+      .link {
+        text-decoration: none;
+        font-size: 30px;
+        color: black;
+      }
+
+    </style>
   </head>
   <body>
   <?php
   include('navbar.php');
 ?>
-    <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="row row-cols-1 row-cols-md-2 g-4" style="padding-top:5rem">
       <div class="col">
-        <div class="card">
-          <br />
+        <div id="card">
+		<br>
           <img
             src="carson.jpg"
             style="
@@ -30,33 +54,35 @@
               display: block;
               margin-left: auto;
               margin-right: auto;
-              border-radius: 10rem;
+			  border-radius: 10rem;
             "
           />
-          <br />
+		  <br>
           <h2>Carson Rodrigues</h2>
-          <p class="rollno">1914010</p>
+          <p id="rollno">1914010</p>
           <h4>Don Bosco College of Engineering</h4>
           <div style="margin: 24px">
-            <a href="https://github.com/rodriguescarson" class="github"
-              ><i class="fa fa-github-square" style="color: #ffffff"></i
-            ></a>
-            &nbsp &nbsp
+			<a 
+				href="https://github.com/rodriguescarson" 
+				class="github link"
+				><i class="fa fa-github-square" style="color:#FFFFFF"></i
+			></a>
+			&nbsp &nbsp
             <a
               href="https://www.linkedin.com/in/rodriguescarson/"
-              class="linkedin"
+              class="linkedin link"
               ><i class="fa fa-linkedin" style="color: #0e76a8"></i
             ></a>
-            &nbsp &nbsp
+			&nbsp &nbsp
             <a href="https://api.whatsapp.com/send/?phone=917020286635"
-              ><i class="fa fa-whatsapp" style="color: #25d366"></i
+              ><i class="fa fa-whatsapp link" style="color: #25d366"></i
             ></a>
           </div>
         </div>
       </div>
       <div class="col">
-        <div class="card">
-          <br />
+        <div id="card">
+		<br>
           <img
             src="yash.jpg"
             style="
@@ -64,33 +90,35 @@
               display: block;
               margin-left: auto;
               margin-right: auto;
-              border-radius: 10rem;
+			  border-radius: 10rem;
             "
           />
-          <br />
+          <br>
           <h2>Yash Karapurkar</h2>
-          <p class="rollno">1914057</p>
+          <p id="rollno">1914057</p>
           <h4>Don Bosco College of Engineering</h4>
           <div style="margin: 24px">
-            <a href="https://github.com/whyKara" class="github"
-              ><i class="fa fa-github-square" style="color: #ffffff"></i
-            ></a>
-            &nbsp &nbsp
+			<a 
+				href="https://github.com/whyKara" 
+				class="github link"
+				><i class="fa fa-github-square" style="color:#FFFFFF"></i
+			></a>
+			&nbsp &nbsp
             <a
               href="https://www.linkedin.com/in/yash-karapurkar-59b92b1b7/"
-              class="linkedin"
+              class="linkedin link"
               ><i class="fa fa-linkedin" style="color: #0e76a8"></i
             ></a>
-            &nbsp &nbsp
+			&nbsp &nbsp
             <a href="https://api.whatsapp.com/send/?phone=917264964649"
-              ><i class="fa fa-whatsapp" style="color: #25d366"></i
+              ><i class="fa fa-whatsapp link" style="color: #25d366"></i
             ></a>
           </div>
         </div>
       </div>
       <div class="col">
-        <div class="card">
-          <br />
+        <div id="card">
+		<br>
           <img
             src="nihal.jfif"
             style="
@@ -98,33 +126,35 @@
               display: block;
               margin-left: auto;
               margin-right: auto;
-              border-radius: 10rem;
+			  border-radius: 10rem;
             "
           />
-          <br />
+          <br>
           <h2>Nihal Kamat</h2>
-          <p class="rollno">1914024</p>
+          <p id="rollno">1914024</p>
           <h4>Don Bosco College of Engineering</h4>
           <div style="margin: 24px">
-            <a href="https://github.com/starix2436" class="github"
-              ><i class="fa fa-github-square" style="color: #ffffff"></i
-            ></a>
-            &nbsp &nbsp
+			<a 
+				href="https://github.com/starix2436" 
+				class="github link"
+				><i class="fa fa-github-square" style="color:#FFFFFF"></i
+			></a>
+			&nbsp &nbsp
             <a
               href="https://www.linkedin.com/in/nihalkamat2436/"
-              class="linkedin"
+              class="link linkedin"
               ><i class="fa fa-linkedin" style="color: #0e76a8"></i
             ></a>
-            &nbsp &nbsp
-            <a href="https://api.whatsapp.com/send/?phone=919405920393"
+			&nbsp &nbsp
+            <a href="https://api.whatsapp.com/send/?phone=919405920393" class="link"
               ><i class="fa fa-whatsapp" style="color: #25d366"></i
             ></a>
           </div>
         </div>
       </div>
       <div class="col">
-        <div class="card">
-          <br />
+        <div id="card">
+		<br>
           <img
             src="histon.jpg"
             style="
@@ -132,25 +162,27 @@
               display: block;
               margin-left: auto;
               margin-right: auto;
-              border-radius: 10rem;
+			  border-radius: 10rem;
             "
           />
-          <br />
+          <br>
           <h2>Histon Pango</h2>
-          <p class="rollno">1914029</p>
+          <p id="rollno">1914029</p>
           <h4>Don Bosco College of Engineering</h4>
           <div style="margin: 24px">
-            <a href="https://github.com/Hestonpango" class="github"
-              ><i class="fa fa-github-square" style="color: #ffffff"></i
-            ></a>
-            &nbsp &nbsp
+			<a 
+				href="https://github.com/Hestonpango" 
+				class="github link"
+				><i class="fa fa-github-square" style="color:#FFFFFF"></i
+			></a> 
+			&nbsp &nbsp
             <a
               href="https://www.linkedin.com/in/histon-pango-34797718b/"
-              class="linkedin"
+              class="linkedin link"
               ><i class="fa fa-linkedin" style="color: #0e76a8"></i
             ></a>
-            &nbsp &nbsp
-            <a href="https://api.whatsapp.com/send/?phone=918379080567"
+			&nbsp &nbsp
+            <a href="https://api.whatsapp.com/send/?phone=918379080567" class="link"
               ><i class="fa fa-whatsapp" style="color: #25d366"></i
             ></a>
           </div>
