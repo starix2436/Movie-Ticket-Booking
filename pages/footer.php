4199223609829724317f8d6a816d7d08ff6e59d3
Carson <rodriguescarson@hotmail.com>
2021-12-29 19:54:58 +0530

    added:pages

--- pages/footer.php
+++ pages/footer.php
@@ -45,24 +45,19 @@
   <footer class="py-5">
     <div class="row">
       <div class="col">
-        <h5>Section</h5>
+        <h5>Box Office</h5>
         <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          <li class="nav-item mb-2"><a href="../index.php" class="nav-link p-0 text-muted">Home</a></li>
+          <li class="nav-item mb-2"><a href="./faq.php" class="nav-link p-0 text-muted">FAQ</a></li>
+          <li class="nav-item mb-2"><a href="./about.php" class="nav-link p-0 text-muted">ABOUT</a></li>
         </ul>
       </div>
 
       <div class="col">
-        <h5>Section</h5>
+        <h5>Extras</h5>
         <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+          <li class="nav-item mb-2"><a href="./developers.php" class="nav-link p-0 text-muted">DEVELOPERS</a></li>
+          <li class="nav-item mb-2"><a href="./allmovies.php" class="nav-link p-0 text-muted">ALL MOVIES</a></li>
         </ul>
       </div>
 
@@ -72,7 +67,7 @@
         <form>
           
           <h5>Subscribe to our newsletter</h5>
-          <p>Monthly digest of whats new and exciting from us.</p>
+          <p>Monthly digest of all upcoming movies</p>
           <div class="d-flex w-100 gap-2 row">
             <label for="newsletter1" class="visually-hidden">Email address</label>
             <input id="newsletter1" class="col-12" type="text" class="form-control" placeholder="Email address">
@@ -83,12 +78,7 @@
     </div>
 
     <div class="d-flex justify-content-between py-4 my-4 border-top">
-      <p>&copy; 2021 Company, Inc. All rights reserved.</p>
-      <ul class="list-unstyled d-flex">
-        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
-        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
-        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
-      </ul>
+      <p>&copy; 2021 BookMyMovie, Inc. All rights reserved.</p>
     </div>
   </footer>
 </div>
